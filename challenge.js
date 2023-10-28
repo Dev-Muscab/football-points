2d18c19455f745691401d08e7f3e451d06e35ff6
muscabinhoyare@gmail.com <muscab-11>
2023-10-28 10:41:23 +0300

    Done Exercise

--- challenge.js
+++ challenge.js
@@ -1,2 +1,6 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+function calculatePoints(win, draw, loss) {
+    
+    var points = (win * 3) + (draw * 1);
+    console.log("The team has earned " + points + " points.");
+  }
+  calculatePoints(10, 6, 3);
